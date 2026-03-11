3296551d4b9a4ceaa8c85ca14a9c59304430baab
Naveen Vikram R <naveenvikram27@gmail.com>
2026-03-11 20:20:16 +0530

    Add solution for adagrad-optimizer

---
+++ adagrad-optimizer/adagrad-optimizer.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def adagrad_step(w, g, G, lr=0.01, eps=1e-8):
+    """
+    Perform one AdaGrad update step.
+    """
+    
+    w = np.array(w)
+    g = np.array(g)
+    G = np.array(G)
+
+    # accumulate squared gradients
+    G = G + g**2
+
+    # parameter update
+    w = w - lr * g / np.sqrt(G + eps)
+
+    return w, G
